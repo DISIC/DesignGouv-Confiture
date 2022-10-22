4eed5069e40d2d32eaacbc09c300a7377922f627
Olivier JAVAUX <olivier.javaux@ovhcloud.com>
2022-10-22 13:11:00 +0000

    in progress

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.get('/', function(req, res) {
     console.log('Get /');
     res.send({
         status: "success",
-        message: "Hello Confiture"
+        message: "Hello Confiture !"
     });
 });
 
